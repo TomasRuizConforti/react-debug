173780ac8e68a2da5e12ef7fd04ac53a68140e5f
4B31 <90332493+candelaochoa@users.noreply.github.com>
2021-09-29 14:57:07 -0300

    Parte final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,17 +23,45 @@ const transformarLogos = logos.map(({ textoAlternativo, srcDelLogo }) => (
   </li>
 ));
 
+const endLogos = [
+  {
+    textoAlternativo: "cineAr",
+    srcDelLogo: "http://cine.ar/img/logo-cinear.svg",
+  },
+  {
+    textoAlternativo: "ARSAT",
+    srcDelLogo: "http://cine.ar/img/logo-arsat.svg",
+  },
+  {
+    textoAlternativo: "INCAA",
+    srcDelLogo: "http://cine.ar/img/logo-incaa.svg",
+  },
+];
+
+const transformarEndLogos = endLogos.map(({ textoAlternativo, srcDelLogo }) => (
+  <li className="li-endLogo">
+    <a className="a-endLogo" href="/">
+      <img className="img-endLogo" src={srcDelLogo} alt={textoAlternativo} />
+    </a>
+  </li>
+));
+
 const PieDePagina = () => {
   return (
     <footer>
       <div className="logosCineAr">
         <ul>{transformarLogos}</ul>
       </div>
-      <div className="DDA">
-        <p>
-          Todos los derechos reservados 2003-2021. República Argentina. INCAA
-        </p>
-        <p>Lima 319; Código Postal: C1073AAG, CABA; República Argentina.</p>
+      <div className="parte2">
+        <div className="DDA">
+          <p>
+            Todos los derechos reservados 2003-2021. República Argentina. INCAA
+          </p>
+          <p>Lima 319; Código Postal: C1073AAG, CABA; República Argentina.</p>
+        </div>
+        <div className="endLogos">
+          <ul>{transformarEndLogos}</ul>
+        </div>
       </div>
     </footer>
   );
